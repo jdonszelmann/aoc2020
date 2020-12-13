97c88277fd8cb5812deebdeb724d81b79222a8c9
jonay2000 <jonabent@gmail.com>
2020-12-13 07:36:49 +0100

    day 13

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ mod day9;
 mod day10;
 mod day11;
 mod day12;
+mod day13;
 
 use clap::{App, Arg};
 
@@ -56,6 +57,7 @@ fn main() {
                 day10,
                 day11,
                 day12,
+                day13,
         ) 
     }
 }
